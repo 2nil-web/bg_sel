0afff8723a05ed8aca33c73e80b13355d510c40d
2nil <casalalanne@free.fr>
2023-04-23 20:41:11 +0200

    Checked in by casal from PC-Denis on Sun Apr 23 20:41:11 CEST 2023

--- chgbg/version.h
+++ chgbg/version.h
@@ -1,1 +1,1 @@
-LPCTSTR name=TEXT("chgbg"), version=TEXT("Unknown_version"), decoration=TEXT("Windows shortcut supercharged."), commit=TEXT("Unknown_commit"), created_at=TEXT("2023-04-23T11:07:06Z");
+LPCTSTR name=TEXT("chgbg"), version=TEXT("Unknown_version"), decoration=TEXT("Windows shortcut supercharged."), commit=TEXT("2dc18b0"), created_at=TEXT("2023-04-23T19:31:42Z");
