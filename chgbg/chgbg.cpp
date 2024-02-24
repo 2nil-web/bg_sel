af302245f88d178d59882334f655987d2f901ce1
2nil <casalalanne@free.fr>
2024-02-24 20:14:30 +0100

    Checked in by casal from PC-Denis on Sat Feb 24 20:14:29 CET 2024

--- chgbg/chgbg.cpp
+++ chgbg/chgbg.cpp
@@ -1,5 +1,6 @@
 
 #include <windows.h>
+#include <shellapi.h>
 #include <shlwapi.h>
 #include <shobjidl.h>
 #include <strsafe.h>
@@ -27,7 +28,7 @@ void MonitInfo() {
   unsigned int n;
   if (SUCCEEDED(pdw->GetMonitorDevicePathCount(&n))) {
     unsigned int i;
-    LPTSTR id[MAX_STR], wp[MAX_STR];
+    LPWSTR  id[MAX_STR], wp[MAX_STR];
     RECT rc;
     wprintf(L"monitors info:\n");
 
@@ -35,11 +36,11 @@ void MonitInfo() {
       if (SUCCEEDED(pdw->GetMonitorDevicePathAt(i, id))) {
         wprintf(L"  number %d\n    id: %ls\n", i, (LPTSTR)*id);
 
-        if (SUCCEEDED(pdw->GetWallpaper((LPCTSTR)*id, wp))) {
+        if (SUCCEEDED(pdw->GetWallpaper(*id, wp))) {
           wprintf(L"    name: %ls\n", (LPTSTR)*wp);
         }
 
-        if (SUCCEEDED(pdw->GetMonitorRECT ((LPCTSTR)*id, &rc))) {
+        if (SUCCEEDED(pdw->GetMonitorRECT (*id, &rc))) {
           wprintf(L"    rect: left %d, top %d, right %d bottom %d\n", rc.left, rc.top, rc.right, rc.bottom);
         }
       }
@@ -47,7 +48,7 @@ void MonitInfo() {
   }
 }
 
-size_t StringLength(const TCHAR *s) {
+size_t StringLength(TCHAR *s) {
   size_t l;
   StringCchLength(s, STRSAFE_MAX_CCH, &l);
   return l;
@@ -55,20 +56,22 @@ size_t StringLength(const TCHAR *s) {
 
 #define die(msg) { win_error(msg); return 0; }
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+#ifdef _MSC_VER
+#    pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+//int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+int main(int argc, char **argv) {
   if (FAILED(CoInitialize(NULL))) die("CoInitialize");
   if (FAILED(CoCreateInstance(__uuidof(DesktopWallpaper), NULL, CLSCTX_LOCAL_SERVER, __uuidof(IDesktopWallpaper), (void**)&pdw))) die("CoCreateInstance");
 
-  int ac, i, j;
   bool ci=false, mi=false;
-  LPTSTR *av=CommandLineToArgvW(GetCommandLine(), &ac);
 
-  for (i=0; i < ac; i++) {
-    if (av[i][0]=='-') {
-      size_t l=StringLength(av[i]);
-      for(j=1; j < l; j++) {
-        if (av[i][j]=='n') ci=true;
-        else if (av[i][j]=='m') mi=true;
+  for (int i=0; i < argc; i++) {
+    if (argv[i][0]=='-') {
+      size_t l=strlen(argv[i]);
+      for(size_t j=1; j < l; j++) {
+        if (argv[i][j]=='n') ci=true;
+        else if (argv[i][j]=='m') mi=true;
       }
     }
   }
